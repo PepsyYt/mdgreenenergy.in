a32d388448c0d8a40e794701a9992f78d3efa16d
PepsyYt <siddharth20042004@gmail.com>
2024-04-23 00:13:16 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: BizLand
-* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+* Template Name: MdGreenEnergy
+* Template URL: https://bootstrapmade.com/MdGreenEnergy-bootstrap-business-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
+Thanks for downloading this template!
 
+Template Name: MdGreenEnergy
+Template URL: https://bootstrapmade.com/MdGreenEnergy-bootstrap-business-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
